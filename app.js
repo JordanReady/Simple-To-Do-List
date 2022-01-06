574dc2fdbbba28e8225d9d131cfdad463eb3c8a0
JordanReady <jready112@gmail.com>
2022-01-06 17:17:09 -0600

    create a new task and display the latest state of all the tasks

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 $(document).ready(function(){
-    $.ajax({
-        type: 'GET',
-        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-        dataType: 'json',
-        success: function (response, textStatus) {
-        response.tasks.forEach(function (task) {
-            $('#todo-list').append('<p>' + task.content + "</p>");
+    var getAllTasks = function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=221',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                $('#todo-list').empty();
+                response.tasks.forEach(function (task) {
+                $('#todo-list').append('<p>' + task.content + "</p>");
+            });
+            },
+            error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+            }
+        });       
+    }
+
+    var createTask = function () {
+        $.ajax({
+            type: 'POST',
+            url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=221',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                task: {
+                    content: $('#new-task-content').val()
+                }
+            }),
+            success: function (response, textStatus) {
+                $('#new-task-content').val(''); 
+                getAllTasks();
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
         });
-        },
-        error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-        }
-    });
+    }
+    $('#create-task').on('submit', function  (e) {
+        e.preventDefault();
+        createTask();
+    })
+
+    getAllTasks();
+
 }); 
