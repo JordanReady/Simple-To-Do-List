730a14adee1196ff4f76004c1bc4dda13222b129
JordanReady <jready112@gmail.com>
2022-01-06 16:48:27 -0600

    inject tasks to DOM

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Testing testing 1 2 3');
+$(document).ready(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+        dataType: 'json',
+        success: function (response, textStatus) {
+        response.tasks.forEach(function (task) {
+            $('#todo-list').append('<p>' + task.content + "</p>");
+        });
+        },
+        error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+        }
+    });
+}); 
